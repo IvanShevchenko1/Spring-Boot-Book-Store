6a7318c27eeabc3dc01f035403ecbc06ebd0f5c2
Ivanov <ivanshevchenko181@gmail.com>
2025-09-02 14:22:28 +0300

    Initial commit

---
+++ src/test/java/org/store/springbootbookstore/SpringBootBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.store.springbootbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
